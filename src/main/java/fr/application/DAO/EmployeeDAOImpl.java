724bed56681f4254b9aa9fec4a586ad91ba2118a
Hocine Hammar <h.hocinee@hotmail.fr>
2019-04-19 15:18:38 +0200

    test

--- src/main/java/fr/application/DAO/EmployeeDAOImpl.java
+++ src/main/java/fr/application/DAO/EmployeeDAOImpl.java
@@ -17,88 +17,110 @@ public class EmployeeDAOImpl implements EmployeeDAO {
 	Connection con = null;
 	private static String URL = "jdbc:mysql://localhost:3306/employee";
 	private static String LOGIN = "root";
-	private static String PASSWORD = "123456";
+	private static String PASSWORD = "root";
 
-	public Employee find(int id) throws Exception {
-
-		Connection con;
-		
-		con = DriverManager.getConnection(URL, LOGIN, PASSWORD);
-
-		Statement stmt = con.createStatement();
-		// ResultSet rset = stmt.executeQuery("SELECT * FROM employee where EMP_ID =
-		// 'id'");
-
-		PreparedStatement pst = (PreparedStatement) con.prepareStatement("SELECT * FROM employee where EMP_ID = ?");
-		Employee emp = null;
-		pst.setInt(1, id);
-		ResultSet rset = pst.executeQuery();
-		if (rset.next()) {
-			emp = new Employee(id);
-			emp.setName(rset.getString("EMP_NAME"));
-			emp.setSalary(rset.getInt("SALARY"));
-
-		}
-		rset.close();
-		stmt.close();
-		con.close();
-
-		return emp;
-
-	}
-
-	public List<Employee> findBigestSalary(int salary) throws SQLException {
-
-		Connection con;
-
-		con = DriverManager.getConnection(URL, LOGIN, PASSWORD);
-
-		Statement stmt = con.createStatement();
-		
-
-		PreparedStatement pst = (PreparedStatement) con.prepareStatement("SELECT * FROM employee where SALARY>?");
-
-		pst.setInt(1, salary);
-		ResultSet rset = pst.executeQuery();
-		while (rset.next()) {
-
-			salary = rset.getInt("SALARY");
-			String name = rset.getString("EMP_NAME");
-			System.out.println("Mr " + name + "touche " + salary + " euros.");
-			
-		}
-		rset.close();
-		stmt.close();
-		con.close();
-
-		return null ;
-	}
 	
-	public List<Employee> findAll() throws SQLException{
+	public Employee find(int id) {
+		Employee emp = null;
+		Statement stmt = null;
+		Connection con = null;
+		PreparedStatement pst;
+		ResultSet rset = null;
 		
-		Connection con;
-
-		con = DriverManager.getConnection(URL, LOGIN, PASSWORD);
-
-		Statement stmt = con.createStatement();
-		
-
-		PreparedStatement pst = (PreparedStatement) con.prepareStatement("SELECT * FROM employee");
-
-		
-		ResultSet rset = pst.executeQuery();
-		while (rset.next()) {
-
-			String name = rset.getString("EMP_NAME");
-			System.out.println("Mr " + name );
-			
+		try {
+			con = DriverManager.getConnection(URL, LOGIN, PASSWORD);
+			stmt = con.createStatement();
+			pst = (PreparedStatement) con.prepareStatement("SELECT * FROM employee where EMP_ID = ?");
+			pst.setInt(1, id);
+			rset = pst.executeQuery();
+			if (rset.next()) {
+				emp = new Employee(id);
+				emp.setName(rset.getString("EMP_NAME"));
+				emp.setSalary(rset.getInt("SALARY"));
+			}
+		} catch (SQLException e) {
 		}
-		rset.close();
-		stmt.close();
-		con.close();
+		finally {
+			try {
+				if(rset!=null)
+				rset.close();
+				if(stmt!=null)
+					stmt.close();
+				if(con!=null)
+					con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return emp;
+	}
+	public List<Employee> findBigestSalary(int salary) {
+		List<Employee> list = null;
+		Connection con = null;
+		Statement stmt = null;
+		PreparedStatement pst;
+		ResultSet rset = null;
+		try {
+			con = DriverManager.getConnection(URL, LOGIN, PASSWORD);
+			stmt = con.createStatement();
+			pst = (PreparedStatement) con.prepareStatement("SELECT * FROM employee where SALARY>?");
+			pst.setInt(1, salary);
+			rset = pst.executeQuery();
+			while (rset.next()) {
+				salary = rset.getInt("SALARY");
+				String name = rset.getString("EMP_NAME");
+				System.out.println("Mr " + name + "touche " + salary + " euros.");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rset != null)
+					rset.close();
+				if (stmt != null)
+					stmt.close();
+				if (con != null)
+					con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return list;
+	}
 
-		
-		return null ;
+	public List<Employee> findAll() {
+		List<Employee> list = null;
+		Connection con = null;
+		Statement stmt = null;
+		ResultSet rset = null;
+		PreparedStatement pst;
+		try {
+			con = DriverManager.getConnection(URL, LOGIN, PASSWORD);
+			stmt = con.createStatement();
+			pst = (PreparedStatement) con.prepareStatement("SELECT * FROM employee");
+			rset = pst.executeQuery();
+			while (rset.next()) {
+				String name = rset.getString("EMP_NAME");
+				System.out.println("M " + name);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (con != null)
+					con.close();
+				if (rset != null)
+					rset.close();
+				if (stmt != null)
+					stmt.close();
+			} catch (SQLException e) {
+				System.out.println("voir les logs");
+			}
+
+		}
+
+		return list;
 	}
 
 }
--- src/main/java/fr/application/Interface/EmployeeDAO.java
+++ src/main/java/fr/application/Interface/EmployeeDAO.java
@@ -1,12 +1,9 @@
 package fr.application.Interface;
 
-import java.sql.SQLException;
 import java.util.List;
-
 import fr.application.hocine2.Employee;
-
 public interface EmployeeDAO {
-Employee find(int id) throws Exception;
-List<Employee> findBigestSalary(int salary) throws SQLException;
-List<Employee> findAll() throws SQLException;
+Employee find(int id);
+List<Employee> findBigestSalary(int salary);
+List<Employee> findAll();
 }
